9beb01bd9c942dca7dced4027f490bf548650c67
Brendan Gaynor <gaynorbrendan@gmail.com>
2012-06-07 14:24:06 -0400

    Initial Commit

---
+++ hallowbagTests/hallowbagTests.h
@@ -0,0 +1,13 @@
+//
+//  hallowbagTests.h
+//  hallowbagTests
+//
+//  Created by Brendan Gaynor on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface hallowbagTests : SenTestCase
+
+@end
---
+++ hallowbagTests/hallowbagTests.m
@@ -0,0 +1,32 @@
+//
+//  hallowbagTests.m
+//  hallowbagTests
+//
+//  Created by Brendan Gaynor on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "hallowbagTests.h"
+
+@implementation hallowbagTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in hallowbagTests");
+}
+
+@end
